5afc9fc1e3a96f76ca716c1d0c4007368733c4d5
Isabelagenuino <isabelagenuinodeoliveira@gmail.com>
2025-05-20 18:01:33 -0300

    Create Questao_03.js

---
+++ Questao_03.js
@@ -0,0 +1,23 @@
+var qtd=3;
+
+var nomeAluno = [3];
+nomeAluno[0]= "João da Silva";
+nomeAluno[1]= "Carolina de Jesus";
+nomeAluno[2]= "Marcos Santos";
+
+var primeiraNota = [3];
+primeiraNota[0]= 7;
+primeiraNota[1]= 5;
+primeiraNota[2]= 9;
+
+var segundaNota = [3];
+segundaNota[0]=8.5;
+segundaNota[1]=2;
+segundaNota[2]=10;
+
+for (var i=0; i < qtd; i++){
+var resultadoP = primeiraNota[i]*0.6;
+var resultadoS = segundaNota[i]*0.4;
+var soma = resultadoP + resultadoS;
+alert(soma);
+}
