b8f0d3962cdf5b4dc82ccec2a539b3a1388bc771
Isabelagenuino <isabelagenuinodeoliveira@gmail.com>
2025-05-20 21:54:30 -0300

    .

--- Questao_04.js
+++ Questao_04.js
@@ -1,11 +1,20 @@
 Class Aluno{
   var primeiroNome,segundoNome,primeiraNota,segundaNota;
   public nomeCompleto (primeiroNome,segundoNome){
-    
+    return primeiroNome +" "+ segundoNome;
   }
+
   public media(){
-    
+    return (primeiraNota * 0.6) + (segundaNota*0.4);
   }
-  public situacao(){}
+
+  public situacao(){
+    if(media>6){
+      return "Aprovado";
+    }
+    else{
+      return "Reprovado";
+    }
+  }
 
 }
