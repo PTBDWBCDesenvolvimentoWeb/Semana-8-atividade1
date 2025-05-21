9870f6eb1a2946edbc3bf3f599646a3b3b132cf3
Isabelagenuino <isabelagenuinodeoliveira@gmail.com>
2025-05-20 21:58:20 -0300

    .

--- Questao_04.js
+++ Questao_04.js
@@ -17,4 +17,16 @@ Class Aluno{
     }
   }
 
+var aluno1 = new Aluno("Carlos", "Souza", 10, 8);
+var aluno2 = new Aluno("Maria", "da Silva", 4, 6);
+var aluno3 = new Aluno("Luis", "Eduardo", 3, 8);
+var aluno4 = new Aluno("Vinicius", "Nogueira", 9, 5);
+var aluno5 = new Aluno("Roberto", "Oliveira", 4, 9);
+var alunos = [aluno1, aluno2, aluno3, aluno4, aluno5];
+
+mostrarAlunos(){
+    for(let aluno of alunos){
+        alert("Nome do aluno: ["+aluno.nomeCompleto()+"] Média: ["+aluno.media().toFixed(2)+"] Situação: "+aluno.situacao());
+    }
+}
 }
