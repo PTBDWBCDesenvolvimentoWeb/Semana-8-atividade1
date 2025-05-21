58821f79eaef091308b4059702c3c9d681ce79b8
Isabelagenuino <isabelagenuinodeoliveira@gmail.com>
2025-05-20 21:24:35 -0300

    Update Questao_03.js

--- Questao_03.js
+++ Questao_03.js
@@ -15,8 +15,9 @@ segundaNota[1]=2;
 segundaNota[2]=10;
 
 for (var i=0; i < 3; i++){
-var resultadoP = primeiraNota[i]*0.6;
-var resultadoS = segundaNota[i]*0.4;
-var soma = resultadoP + resultadoS;
-alert("Nome do aluno\n"+nomeAluno[i]+"Nota1:\n"+primeiraNota[i]+,"Nota2:\n"+segundaNota[i]);
+var resultadoP[i] = primeiraNota[i]*0.6;
+var resultadoS[i] = segundaNota[i]*0.4;
+var soma[i] = resultadoP[i] + resultadoS[i];
+alert("Nome do aluno:"/n+nomeAluno[i]+"Nota1:\n"+primeiraNota[i]+,"Nota2:\n"+segundaNota[i]);
+alert("Nome do aluno:"/n+nomeAluno[i]+"Nota final:"+soma[i]);
 }
